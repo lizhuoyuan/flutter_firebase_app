f6399548ab83529c39ada01f9ea3a71633c4731f
LiZhuoyuan <zhuoyuan93@gmail.com>
2020-08-12 23:33:13 +0800

    Update Application.java

--- android/app/src/main/java/com/zy/flutter_firebase_app/Application.java
+++ android/app/src/main/java/com/zy/flutter_firebase_app/Application.java
@@ -1,4 +1,6 @@
 package com.zy.flutter_firebase_app;
 
 public class Application {
+//        FirebaseMessagingPlugin.registerWith(registry.registrarFor("io.flutter.plugins.firebasemessaging.FirebaseMessagingPlugin()"));
+
 }
